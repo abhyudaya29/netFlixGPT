11f46fb8303ab0bfa1e5810e0a5ed4edf309f4e5
abhyudaya29 <abhyudayadev123@gmail.com>
2024-01-22 14:05:11 +0530

    Setup Header Logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+   <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 opacity-65">
+    <img className="w-44" src="https://images8.alphacoders.com/115/1152301.png" alt="netflix logo" />
+   </div>
+   
+    </>
+  )
+}
+
+export default Header
