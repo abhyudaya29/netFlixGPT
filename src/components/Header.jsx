97b642ad7471bceff74cf322cd2dad092a91af0d
abhyudaya29 <abhyudayadev123@gmail.com>
2024-01-25 07:18:53 +0530

    header full width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-   <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 opacity-65">
+   <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 opacity-65">
     <img className="w-44" src="https://images8.alphacoders.com/115/1152301.png" alt="netflix logo" />
    </div>
    
