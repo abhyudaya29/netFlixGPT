033e0cf46e05f028cf22e7149e8ce7b23a4fb408
abhyudaya29 <abhyudayadev123@gmail.com>
2024-01-25 07:19:46 +0530

    new Features

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,17 @@
 
 import Login from './Login'
 import Browse from './Browse'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, useNavigate } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
+import { useEffect } from 'react'
+import {  onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/fireBase'
+import { useDispatch } from 'react-redux'
+import { addUser, removeUser } from '../utils/userSlice'
+
 const Body = () => {
+  const dispatch=useDispatch();
+  
   const appRouter=createBrowserRouter([
     {
       path:'/',
@@ -14,6 +22,25 @@ const Body = () => {
       element:<Browse/>
     }
   ])
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // if user Sign Up
+        const{uid,email,displayName}=user;
+        dispatch(addUser({uid:uid,email:email,displayName:displayName}))
+        // if user SignUp then take him/her to browse page
+        // navigate('/browse')
+
+        // ...
+      } else {
+        // User is signed out
+        dispatch(removeUser())
+        // navigate('/')
+      }
+    });
+    
+
+  },[])
   return (
     <>
     <RouterProvider router={appRouter}/>
