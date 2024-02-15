f35556c48a731d7cd185d725a61d6a750edc21dd
abhyudaya29 <abhyudayadev123@gmail.com>
2024-02-15 20:38:05 +0530

    changing openapi export name

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+import 'dotenv'
+
 export const LOGO = "https://images8.alphacoders.com/115/1152301.png";
 export const USER_AVTAR = "https://avatars.githubusercontent.com/u/90976445?v=4";
 
@@ -13,7 +15,7 @@ export const IMG_CDN = "https://image.tmdb.org/t/p/w200";
 export const BG_URL = "https://wallpapers.com/images/hd/netflix-background-gs7hjuwvv2g0e9fj.jpg";
 
 // Use correct import based on framework:
-export const OPEN_AI_KEY =import.meta.env.VITE_REACT_APP_OPENAI_KEY
+export const OPENAI_KEY =import.meta.env.VITE_REACT_APP_OPENAI_KEY
   // OR
   // For Create React App:
   // process.env.REACT_APP_OPENAI_KEY;
--- src/utils/openAi.js
+++ src/utils/openAi.js
@@ -1,8 +1,8 @@
 import OpenAI from 'openai';
-import { OPEN_AI_KEY } from './constants';
+import { OPENAI_KEY } from './constants';
 
 const openai = new OpenAI({
-  apiKey:OPEN_AI_KEY, // This is the default and can be omitted
+  apiKey:OPENAI_KEY, // This is the default and can be omitted
   dangerouslyAllowBrowser:true
 });
 
